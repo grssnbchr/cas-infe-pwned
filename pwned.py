1220854152fbae1b1a817c23833f7f3f180df1c1
Phlogi <phlogi1@gmail.com>
2018-12-17 18:13:41 +0100

    add 64M pw file

---
+++ pwned.py
@@ -0,0 +1,19 @@
+# Constants
+BUCKET_PATH = ''
+
+
+from pyspark import SparkConf, SparkContext, SQLContext
+from pyspark.sql.types import StructType, StructField, StringType, FloatType
+
+from pyspark.context import SparkContext
+
+sc = SparkContext()
+# spark = SparkSession(sc)
+
+
+
+data = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+dataRDD = sc.parallelize(data, 2)
+dataRDD.cache()
+print('Count: %s ' % dataRDD.count())
+print( 'Take(5): %s' % dataRDD.take(5))
