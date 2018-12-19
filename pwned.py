4c094ad4bef9cd8aeea7bdfe7fe271311c4eb5c0
Timo Grossenbacher <wnstnsmth@yahoo.com>
2018-12-19 15:29:25 +0100

    first 'working' example

--- pwned.py
+++ pwned.py
@@ -1,19 +1,38 @@
+from __future__ import print_function # necessary because Dataproc runs Python 2.7
 # Constants
-BUCKET_PATH = ''
+LOCAL = True # TODO: set dynamically
 
+BUCKET_PATH = 'gs://dataproc-ec96d46c-3f60-46a2-acb4-066fe551dff8-europe-west2/'
 
+PWS_PATH = 'first_64M_pwned-passwords-ordered-by-hash.txt/'
+COMMONWORDS_PATH = '10000-most-common-words.txt'
+
+# if script is executed on Dataproc, bucket path needs to be prepended
+if LOCAL is False:
+    PWS_PATH = BUCKET_PATH + PWS_PATH
+    COMMONWORDS_PATH = BUCKET_PATH + COMMONWORDS_PATH
+
+import sys
 from pyspark import SparkConf, SparkContext, SQLContext
-from pyspark.sql.types import StructType, StructField, StringType, FloatType
-
-from pyspark.context import SparkContext
 
 sc = SparkContext()
-# spark = SparkSession(sc)
+
+print(sys.version_info)
+
+print('Spark version %s running.' % sc.version)
+
+# print 'Config values:'
+# print sc.getConf().getAll()
+
+print('Reading in files...')
+
+rawPasswordList = sc.textFile(PWS_PATH)
+rawCommonWordsList = sc.textFile(COMMONWORDS_PATH)
+
+print(rawPasswordList.take(1))
+
+print('Files read.')
 
 
 
-data = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-dataRDD = sc.parallelize(data, 2)
-dataRDD.cache()
-print('Count: %s ' % dataRDD.count())
-print( 'Take(5): %s' % dataRDD.take(5))
+
