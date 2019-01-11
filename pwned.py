f2eb81854e62a1634c8985f4e6bef00824c47d9f
grssnbchr <timo@timogrossenbacher.ch>
2019-01-11 11:01:00 +0100

    cleanup

--- pwned.py
+++ pwned.py
@@ -22,8 +22,8 @@ BUCKET_PATH = 'gs://dataproc-ec96d46c-3f60-46a2-acb4-066fe551dff8-europe-west2/'
 # Password list taken from https://haveibeenpwned.com/Passwords, SHA 1, 9.18 GB, containing over
 # half a billion pwned passwords
 
-# PWS_PATH = 'first_64M_pwned-passwords-ordered-by-hash.txt/'
-PWS_PATH = 'first_512M_pwned-passwords-ordered-by-hash.txt/'
+#PWS_PATH = 'first_64M_pwned-passwords-ordered-by-hash.txt/'
+#PWS_PATH = 'first_512M_pwned-passwords-ordered-by-hash.txt/'
 
 PWS_PATH = 'pwned-passwords-ordered-by-hash.txt'
 COMMONWORDS_PATH = '10000-most-common-words.txt'
@@ -66,12 +66,6 @@ def df_sql_approach():
     # common_words_df = sqlc.createDataFrame(sc.textFile(COMMONWORDS_PATH), schema=common_words_schema)
     common_words_df = sqlc.read.csv(COMMONWORDS_PATH, header=False, schema=common_words_schema, sep='|')
     with time_usage('DF SQL APPROACH'):
-        # print('Head of df:')
-        # print(pw_df.head())
-        # print('Schema of df:')
-        # print(pw_df.printSchema())
-        # print(common_words_df.head())
-        # print(common_words_df.printSchema())
 
         # The hashes of the common words are only needed when comparing
         # Idea: Use a function during sql query that hashes the words on-demand
@@ -84,13 +78,6 @@ def df_sql_approach():
         # print('Example of executing hash function within sql:')
         # print(sqlc.sql("SELECT *, sha1hash(word) as hash FROM common_words_df").take(2))
 
-        # direct function does not work
-        # sha1hash(common_words_df.word) as hash
-        # print(sqlc.sql("""SELECT common_words_df.word, pw_df.h
-        #               FROM common_words_df
-        #               JOIN pw_df ON sha1hash(common_words_df.word) = pw_df.hashedpw""").collect())
-
-        # other approach:
         # we add the hashed column to each word first
         with time_usage('hashing common words by adding an extra column'):
             common_words_df = common_words_df.rdd \
@@ -104,10 +91,15 @@ def df_sql_approach():
         #     print("Count: " + str(j.count()))
 
         print("joining tables...")
-        with time_usage('Joining tables on da hash'):
+        with time_usage('Joining tables on the hash [forced broadcast]'):
             j = broadcast(common_words_df).join(pw_df, common_words_df.hashedword == pw_df.hashedpw) \
                 .select(common_words_df['word'], pw_df['h'])
             j.orderBy("h", ascending=False).show(100)
+
+#        with time_usage('Joining tables on the hash [no broadcast]'):
+#            j = common_words_df.join(pw_df, common_words_df.hashedword == pw_df.hashedpw) \
+#                .select(common_words_df['word'], pw_df['h'])
+#            j.orderBy("h", ascending=False).show(100)
         print("Count: " + str(j.count()))
 
 # Approach using lower level RDD
@@ -117,7 +109,6 @@ def rdd_approach():
     rawPasswordRDD = sc.textFile(PWS_PATH)
     rawCommonWordsRDD = sc.textFile(COMMONWORDS_PATH)
     with time_usage('RDD APPROACH'):
-
 
         print('Files read.')
 
@@ -154,9 +145,6 @@ def rdd_approach():
 
         print('Preprocessing completed.')
 
-        # parsedPasswordRDD.cache()
-        # parsedCommonWordsRDD.cache()
-
         # ---------------------------------- SEARCH
 
         # search for a single word
@@ -166,22 +154,11 @@ def rdd_approach():
         #         .filter(lambda (pw, count): pw == sha1('love').hexdigest())
         #     print('Found %s entries' % res.count())
 
-        # search for a random word in most common words list
-        # insight: if parsedPasswordRDD is cached above, this operation takes factor 10 less time
-        # caching parsedCommonWordsRDD doesn't result in a performance improvement though
-        # with time_usage('Searching for a random most common word'):
-        #     mostCommonWord = parsedCommonWordsRDD.takeSample(False, 1)
-        #     print('Searching for %s' % mostCommonWord[0][0])
-        #     res = parsedPasswordRDD \
-        #         .filter(lambda (pw, count): pw == mostCommonWord[0][1])
-        #     print('Found %s entries' % res.count())
-
         # search if hash exists in most common words
         # for this, the most-common-words rdd needs to be broadcast
         broadcastCommonWordsRDD = sc.broadcast(parsedCommonWordsRDD.map(lambda word: (word[1], word[0])).collectAsMap())
         with time_usage('Searching for every most common word [broadcasted]'):
             # helper function
-
             def search_for_word(word):
                 # print('Searching for %s' % word[0])
                 # broadcastPasswordRDD.value is a dict!
@@ -225,13 +202,14 @@ sc_conf.setAppName("pwned")
 # sc_conf.set('spark.cores.max', '4')
 
 sc_conf.set('spark.sql.crossJoin.enabled', True)
+
 sc = SparkContext(conf=sc_conf)
 sqlc = SQLContext(sc)
 print(sys.version_info)
 
 print('Spark version %s running.' % sc.version)
 
-print('Config values of Schpark context: ')
+print('Config values of Spark context: ')
 print(sc.getConf().getAll())
 
 
@@ -240,4 +218,5 @@ for f in runner:
     f()
 
 print('Finished.')
-
+# hack to keep spark ui alive
+raw_input("Press ctrl+c to exit")
